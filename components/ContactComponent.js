bd086b899ecca9f660f3f43577deb75f1aab59d0
Junalynn Caoli <junalynn14@gmail.com>
2021-06-19 10:54:35 -0700

    Week 1 Workshop Project

---
+++ components/ContactComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Card } from 'react-native-elements';
+import { Text, ScrollView } from 'react-native';
+
+class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card title='Contact Information' wrapperStyle={{ margin: 20 }}>
+                    <Text>
+                        1 Nucamp Way
+                    </Text>
+                    <Text>
+                        Seattle, WA 98001
+                    </Text>
+                    <Text style={{ marginBottom: 10 }}>
+                        U.S.A.
+                    </Text>
+                    <Text>
+                        Phone: 1-206-555-1234
+                    </Text>
+                    <Text>
+                        Email: campsites@nucamp.co
+                    </Text>
+                </Card>
+            </ScrollView >
+        )
+    }
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+}
+
+export default Contact;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Constants from 'expo-constants';
@@ -44,9 +46,45 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
+        About: { screen, AboutNavigator },
+        Contact: { screen, ContactNavtigator },
         Directory: { screen: DirectoryNavigator }
     },
     {
